de5e25c71bf2647eb650eb56c567d8e11d0e642a
ChrisCo <chrisco255@protonmail.com>
2022-11-04 22:13:42 -0500

    adding tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="text-3xl font-bold underline">
           Welcome to <a href="https://nextjs.org">Next.js 13!</a>
         </h1>
 
@@ -53,5 +53,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
